21fd4243c9d22dd29647c95ba154e670412f2afb
willian pereira <williansouza9687a@gmail.com>
2021-07-23 14:06:52 -0300

    pronto

---
+++ script.js
@@ -0,0 +1,58 @@
+let busca = document.querySelector('.busca')
+
+busca.addEventListener('submit', async (event)=>{
+    //previnir comportamento padrao do form
+    event.preventDefault();
+
+    let input = document.querySelector('#searchInput').value;
+
+    if(input !== ''){
+        clearInfo()
+      showWarning('Carregando.....');
+
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=93fc94cb44b9589a2e785353acce4692&units=metric&lang=pt_br
+      `
+      let results = await fetch(url);
+        let json = await results.json();
+
+        if(json.cod === 200){
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            })
+        }else{
+            clearInfo()
+            showWarning('Não encontramos essa Localização')
+        }
+    }
+    
+
+   
+
+})
+
+function showInfo(json){
+    showWarning('')
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`
+    document.querySelector('.tem img').setAttribute('src',` http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle - 90}deg)`
+
+    document.querySelector('.resultado').style.display = 'block'
+
+}
+
+function clearInfo(){
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none'
+
+}
+
+function showWarning(msg){
+    let aviso = document.querySelector('.aviso').innerHTML = msg;
+}
